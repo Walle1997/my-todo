0c0e80162b44caec904f3a8809a1221abafdff7c
Farid Aliev <farid-keltuzed@mail.ru>
2025-03-26 12:38:04 +0300

    Created a project +  Components

---
+++ App.js
@@ -0,0 +1,19 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import TodoList from './components/TodoList';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <TodoList />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ data/todos.js
@@ -0,0 +1,67 @@
+const todosData = [
+    {
+        id: 1,
+        text: 'Приказать мужу выкинуть мусор',
+        isCompleted: true,
+        isToday: true,
+        hour: '10:00',
+    },
+    {
+        id: 2,
+        text: 'Начать готовить обед',
+        isCompleted: false,
+        isToday: false,
+        hour: '12:30',
+    },
+    {
+        id: 3,
+        text: 'Прогулка с мужем',
+        isCompleted: false,
+        isToday: true,
+        hour: '15:00',
+    },
+    {
+        id: 4,
+        text: 'Завершить задачи на сегодня',
+        isCompleted: false,
+        isToday: false,
+        hour: '17:30',
+    },
+    {
+        id: 5,
+        text: 'Продолжить изучение английского',
+        isCompleted: false,
+        isToday: false,
+        hour: '19:00',
+    },
+    {
+        id: 6,
+        text: 'Посетить музей',
+        isCompleted: false,
+        isToday: false,
+        hour: '21:30',
+    },
+    {
+        id: 7,
+        text: 'Продолжить чтение книги',
+        isCompleted: false,
+        isToday: false,
+        hour: '23:00',
+    },
+    {
+        id: 8,
+        text: 'Отдохнуть',
+        isCompleted: false,
+        isToday: false,
+        hour: '00:30',
+    },
+    {
+        id: 9,
+        text: 'Подъем',
+        isCompleted: false,
+        isToday: false,
+        hour: '07:00',
+    }
+];
+
+module.exports = { todosData };
